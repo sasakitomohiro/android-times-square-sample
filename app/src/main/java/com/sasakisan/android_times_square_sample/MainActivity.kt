057a41f87293ebdc9b268d109d8bdff8af14de4d
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2019-05-04 14:57:09 +0900

    first commit

---
+++ app/src/main/java/com/sasakisan/android_times_square_sample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sasakisan.android_times_square_sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.squareup.timessquare.CalendarPickerView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val calendar = findViewById<CalendarPickerView>(R.id.calendar_view)
+        val maxDate = Calendar.getInstance()
+        val minDate = Calendar.getInstance()
+        val today = Date()
+
+        maxDate.apply {
+            set(2100, 12, 1, 0, 0)
+        }
+        minDate.apply {
+            set(1900, 0, 1, 0, 0)
+        }
+
+        calendar.init(minDate.time, maxDate.time)
+            .withSelectedDate(today)
+//            .inMode(CalendarPickerView.SelectionMode.RANGE) // <- これをつけると範囲指定できる
+        calendar.setOnDateSelectedListener(object : CalendarPickerView.OnDateSelectedListener {
+            override fun onDateSelected(date: Date?) {
+                val simpleDateFormat = SimpleDateFormat("yyyy年MM月dd日", Locale.JAPAN)
+                date?.let {
+                    Toast.makeText(this@MainActivity, simpleDateFormat.format(it.time), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onDateUnselected(date: Date?) {
+            }
+        })
+    }
+}
